ca96deb35c2809754e8cc2239cff8c98ef0ba5e7
Pratyush-exe <pratyush21225@gmail.com>
2024-02-07 21:49:16 +0530

    FIX: fixed some issues in task model

--- backend/databases/models/task/task.py
+++ backend/databases/models/task/task.py
@@ -5,10 +5,10 @@ database = SQLAlchemy()
 
 
 class Task(database.Model):
-    table_name = "todo_list"
-    id = database.Column(database.Integer, primary_key=True)
+    __tablename__ = "task"
+    uuid = database.Column(database.String(36), primary_key=True)
     title = database.Column(database.String(100), unique=True, nullable=False)
     description = database.Column(database.String(500), unique=True, nullable=False)
 
     def json(self):
-        return {"id": self.id, "title": self.title, "description": self.description}
+        return {"uuid": self.uuid, "title": self.title, "description": self.description}
